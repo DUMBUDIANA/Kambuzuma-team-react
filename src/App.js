61af0f75730adda0329454a026d1bb494bb9f060
Kelvin <kelvinmusipha@gmail.com>
2024-07-22 05:25:51 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Banner from "../src/pages/Banner";
+import About from "../src/pages/About";
+import Vans from "../src/pages/Vans";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/About">About</Link>
+        </nav>
       </header>
-    </div>
-  );
-}
+      <Routes>
+        <Route path="/" element={<Banner />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+  )
+};
 
-export default App;
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
+
+  export default App;
