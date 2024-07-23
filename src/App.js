d22cfb6a8a318945cf2eed95d64d38e6af951ed7
Your Name <you@example.com>
2024-07-23 14:09:10 +0200

    vans edited

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Banner from "../src/pages/Banner";
+import About from "../src/pages/About";
+import Vans from "../src/pages/Vans";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/About">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Banner />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+  )
+};
+
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
+
+  export default App;
---
+++ src/pages/Vans.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function vans() {
+  return (
+    <div>
+    <h1>Explore our van options</h1>
+    <div>vans exploredxx</div>
+    </div>
+  )
+}
