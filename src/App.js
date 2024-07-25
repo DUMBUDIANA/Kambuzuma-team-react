fbee44b70e060de0b34229036d91ed1f2dc49803
Kelvin <kelvinmusipha@gmail.com>
2024-07-25 13:27:05 +0200

    new styles

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Banner from "../src/pages/Banner";
+import About from "../src/pages/About";
+import Vans from "../src/pages/Vans";
+
+function App() {
+  return (
+
+    <div className='main-section'>
+    <BrowserRouter>
+      <header className='Header-main'>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/About">About</Link>
+          <Link to="/Vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Banner />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+  )
+};
+
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
+
+  export default App;
