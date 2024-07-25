a16beafeaa65a0484f0153950262168d50d608ac
unknown <mututadaniel54@gmail.com>
2024-07-25 00:01:21 -0700

    daniel

---
+++ src/components/Database/Database.jsx
@@ -0,0 +1,100 @@
+
+
+
+const Card = [
+
+    {
+        image:"Rectangle 162.png",
+        type:"Modest Explorer",
+        price:"60",
+        button:"Simple",
+        color:"#E17654",
+        description:"The Modest Explorer is a van designed to get you out of the house and into nature. This    beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
+        rent:"Rent this van",
+        day:"/day",
+        Name:"Name",
+        Category:"Category",
+        Visibility:"Visibility",
+        id:"1"
+    },
+
+    {
+        image:"Rectangle 163.png",
+        type:"Beach Bum",
+        price:"80",
+        button:"Rugged",
+        color:"#115E59",
+        description:"Beach Bum is a van inspired by surfers and travelers. It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.",
+        day:"/day",
+         id:"2"
+    },
+
+    {
+        image:"alpert-wang-rxQL0ieF_Bs-unsplash 2.png",
+        type:"Reliable Red",
+        price:"100",
+        button:"Luxury",
+        color:"#161616",
+        description:"Reliable Red is a van that was made for travelling. The inside is comfortable and cozy, with plenty of space to stretch out in. There's a small kitchen, so you can cook if you need to. You'll feel like home as soon as you step out of it.",
+        day:"/day",
+         id:"3"
+    },
+
+    {
+        image:"image 57.png",
+        type:"Dreamfinder",
+        price:"65",
+        button:"Simple",
+        color:"#E17654",
+        description:"Dreamfinder is the perfect van to travel in and experience. With a ceiling height of 2.1m, you can stand up in this van and there is great head room. The floor is a beautiful glass-reinforced plastic (GRP) which is easy to clean and very hard wearing. A large rear window and large side windows make it really light inside and keep it well ventilated.",
+        day:"/day",
+         id:"4"
+    },
+
+    {
+        image:"Rectangle 155.png",
+        type:"The Cruiser",
+        price:"120",
+        button:"Luxury",
+        color:"#161616",
+        description:"The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior and ample storage space, the Cruiser offers a beautiful view wherever you go.",
+        day:"/day",
+         id:"5"
+    },
+
+    {
+        image:"Rectangle 156.png",
+        type:"Green Wonder",
+        price:"70",
+        button:"Rugged",
+        color:"#115E59",
+        description:"With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.",
+        day:"/day",
+         id:"6"
+    },
+
+    {
+        image2:"image 55.png",
+        button2:"Explore our vans",
+        backgroundcolor2:"#FFCC8D",
+        color2:"#161616",
+        description2:"Don't squeeze in a sedan when you could relax in a van.",
+        text2:"Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.",
+        textbottom:"Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.",
+         id:"7"
+    },
+
+    {
+        image1:"image 54.png",
+        button1:"Find your van",
+        backgroundcolor1:"#FFCC8D",
+        color1:"#FF8C38",
+        description1:"You got the travel plans, we got the travel vans.",
+        text1:"Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.",
+         id:"8"
+    }
+
+
+
+]
+export default Card;
---
+++ src/pages/Banner.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default function Home() {
+    return (
+        <div className="home-container">
+
+            <div className="navbar">
+        <div className="logo">
+            <p>#VANLIFE</p>
+        </div>
+
+        <div className="links">
+            <Link>About</Link>
+            <Link>Vans</Link>
+        </div>
+            </div>
+            <h1>You got the travel plans, we got the travel vans.</h1>
+            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <Link to="vans">Find your van</Link>
+        </div>
+    )
+};
