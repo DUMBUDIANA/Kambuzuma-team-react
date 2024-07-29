10a79842f1558b4072fc476993d4b4d2e4b7636b
Kelvin <kelvinmusipha@gmail.com>
2024-07-29 14:13:21 +0200

    resolved conflicts

--- src/pages/Banner.js
+++ src/pages/Banner.js
@@ -3,16 +3,12 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
-<<<<<<< HEAD
+        <div>
         <div className="home-container">
-    
-           
-
             <h1>You got the travel plans, we got the travel vans.</h1>
             <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
             <Link to="vans">Find your van</Link>
         </div>
-=======
         <div>
         <div className="home-container">
         <div className="section-divide3">
@@ -26,7 +22,7 @@ export default function Home() {
         <p>Ⓒ 2022 #VANLIFE</p>
         </div>
         </div>
+        </div>
    
->>>>>>> 787da4f2ab723b7a1f244c921c0dfaba3b438c77
     )
 };
--- src/pages/Vans.js
+++ src/pages/Vans.js
@@ -1,7 +1,8 @@
 import React from 'react'
-<<<<<<< HEAD
 import CardsStructure from '../components/CardsStructure'
 import Card from '../components/Database/Database'
+
+
 export default function Vans() {
   const Aboutvans = Card.map(item => {
    
@@ -29,12 +30,3 @@ export default function Vans() {
     </div>
   )
 }
-
-=======
-
-export default function vans() {
-  return (
-    <div>vans</div>
-  )
-}
->>>>>>> 787da4f2ab723b7a1f244c921c0dfaba3b438c77
