23d16f75e91de69e76e1df012067f29710bda933
unknown <mututadaniel54@gmail.com>
2024-08-01 04:44:14 -0700

    Your commit message

--- src/pages/Banner.js
+++ src/pages/Banner.js
@@ -3,16 +3,6 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
-<<<<<<< HEAD
-        <div className="home-container">
-    
-           
-
-            <h1>You got the travel plans, we got the travel vans.</h1>
-            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-            <Link to="vans">Find your van</Link>
-        </div>
-=======
         <div>
         <div className="home-container">
         <div className="section-divide3">
@@ -27,6 +17,5 @@ export default function Home() {
         </div>
         </div>
    
->>>>>>> 787da4f2ab723b7a1f244c921c0dfaba3b438c77
     )
 };
