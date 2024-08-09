1177669073f97289f37e7fc336ba8d91c5fddb4d
audreylizzy <lizzyaudreygideon@gmail.com>
2024-08-09 12:04:29 +0200

    filter

--- src/App.js
+++ src/App.js
@@ -6,26 +6,31 @@ import Banner from "../src/pages/Banner";
 import About from "../src/pages/About";
 import Vans from "../src/pages/Vans";
 import logo from "../src/Images/logog.png";
-import Host from './pages/Host';
+import logIn from "../src/Images/User circle.png";
+import CreateAcc from "./pages/SignUp";
+import LogIn from './pages/SignIn';
 import Hoss from './pages/Hoss';
-import "./App.css"
+import "./App.css";
+
 function App() {
+  
   return (  
     <div className='main-section'>
        <BrowserRouter>
       <header className='Header-main'>
         <Link className="site-logo" to="/"><img src={logo} alt= "logo"className="logo" /></Link>
         <nav>
-          <Link to="/About">About</Link>
-          <Link to="/Vans">Vans</Link>
-          <Link to="/Host">Host</Link>
+          <Link to="/About" className='links'>About</Link>
+          <Link to="/Vans" className='links'>Vans</Link>
+           <Link to="/SignIn" className='links'><img src={logIn} alt="login"  className='sign-icon'/></Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Banner />} />
         <Route path="/About" element={<About />}/>
         <Route path="/Vans" element={<Vans />} />
-        <Route path="/Host" element={<Host />} />
+        <Route path="/SignIn" element={<LogIn />} />
+        <Route path="/SignUp" element={<CreateAcc />} />
         <Route path="/Hoss" element={<Hoss />} />
       </Routes>
 
