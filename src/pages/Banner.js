173232ae3019e22778e2ec023955458ae2ce64c0
Kelvin <kelvinmusipha@gmail.com>
2024-10-04 03:34:57 +0200

    added styles on banner page

--- src/pages/Banner.js
+++ src/pages/Banner.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
-        <div >
+        < >
        
         <div className="home-container">
         <div className="section-divide3">
             <h1 className="text-spacing">You got the travel plans, we got the travel vans.</h1>
-            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <p className="banner1-text">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
             <Link to="vans" className="link-banner">Find your van</Link>
         </div>
         
@@ -17,7 +17,7 @@ export default function Home() {
         <p>Ⓒ 2022 #VANLIFE</p>
         </div> 
       
-        </div>
+        </>
    
     )
 };
